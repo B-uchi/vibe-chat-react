874deb1aba930e11600ce54a2aa329df93ce75d1
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-07-14 20:02:45 +0100

    changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
-    <nav className='bg-[#313131] w-full h-[8vh]'>
-        <div className="container mx-auto text-white flex justify-between items-center p-3">
-            <div className="">
-                <h1 className='font-bold font-rowdies text-2xl'>Vibe Chat</h1>
-            </div>
-            <div className="font-poppins">
-                User
-            </div>
+    <nav className="bg-[#313131] w-full h-[8vh]">
+      <div className="container mx-auto text-white flex justify-between items-center p-3">
+        <div
+          onClick={() => {
+            navigate("/");
+          }}
+          className="cursor-pointer"
+        >
+          <h1 className="font-bold font-rowdies text-2xl">Vibe Chat</h1>
         </div>
+        <div className="font-poppins">User</div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
