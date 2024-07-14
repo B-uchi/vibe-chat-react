e3a5f65dfe3dba7528592d0134b8bdb96cec3998
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-07-14 20:03:53 +0100

    Added chats and chatwindow component

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,17 +1,23 @@
-import {  useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Toaster, toast } from "sonner";
+import { FiPlus } from "react-icons/fi";
 
 const Dashboard = () => {
   const navigate = useNavigate();
   const [authLoading, setAuthLoading] = useState(null);
 
-
   return (
-    <div className="relative h-full">
+    <div className="relative h-full flex">
       <Toaster richColors position="top-right" />
-      bbb
-      <div className="absolute right-0 bottom-0"> plus</div>
+      <div className="bg-[#efefef] w-[30%] border-r-[1px] border-r-[#d3d2d2]"></div>
+      <div className="absolute right-10 bottom-10">
+        <div>
+          <button className="shadow-lg p-2 flex justify-center items-center rounded-full bg-[#313131] text-white hover:bg-[#686868] transition-all">
+            <FiPlus size={40} />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
