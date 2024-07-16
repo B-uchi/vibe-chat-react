30fee18c4d589488b1a7e45ed33e5107b100edba
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-07-16 10:25:07 +0100

    added new route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,24 +1,36 @@
-import * as admin from '../util/firebase/config.js'
-// import { getFirestore } from "firebase-admin/firestore";
-// import { getAuth } from "firebase-admin/auth";
+import * as admin from "../util/firebase/config.js";
+import { FieldValue, getFirestore } from "firebase-admin/firestore";
+import { getAuth } from "firebase-admin/auth";
 
-// const db = getFirestore();
-// const auth = getAuth();
+const db = getFirestore();
+const auth = getAuth();
 
 export const setUsername = async (req, res) => {
   console.log("req received to completeSignup");
-//   const { username } = req.body;
-//   try {
-//     const userRef = db.collection("users").doc(req.uid);
-//     await userRef.set(
-//       {
-//         username,
-//         updatedAt: FieldValue.serverTimestamp(),
-//       },
-//       { merge: true }
-//     );
-//     return res.status(200).json({ message: "Success" });
-//   } catch (error) {
-//     return res.status(500).json({ message: error.message });
-//   }
+  const { username, photoId } = req.body;
+  try {
+    const userRef = db.collection("users").doc(req.uid);
+    await userRef.set(
+      {
+        profileData: { username, profilePhoto: photoId || null },
+        updatedAt: FieldValue.serverTimestamp(),
+      },
+      { merge: true }
+    );
+    return res.status(200).json({ message: "Success" });
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ message: error.message });
+  }
 };
+
+export const getUser = async (req, res) => {
+  console.log('req recieved to getUser')
+  try {
+    const userRef = db.collection(users).doc(req.uid)
+    const userData = (await userRef.get()).data()
+    return res.status(200).json({userData})
+  } catch (error) {
+    return res.status(500).json({message:error.message})
+  }
+}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { setUsername } from '../controllers/userController.js'
+import { getUser, setUsername } from '../controllers/userController.js'
 import { verifyToken } from '../middleware/auth.js'
 
 const router = express.Router()
 
 router.post('/completeSignup',verifyToken, setUsername)
+router.get('/getUser',verifyToken, getUser)
 
 export default router
