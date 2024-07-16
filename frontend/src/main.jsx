2c3e2e498f1f9da502c6fef0e32dbae080d6e62b
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-07-16 10:25:33 +0100

    added redux provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AuthProvider } from "./lib/hooks/useAuth.jsx";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
