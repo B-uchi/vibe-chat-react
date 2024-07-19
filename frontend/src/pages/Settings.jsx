08596954027a03bc146045a6683bba0034f01641
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-07-19 21:50:30 +0100

    working on settings page

--- frontend/src/pages/Settings.jsx
+++ frontend/src/pages/Settings.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Settings = () => {
   return (
-    <div>Settings</div>
+    <div className='bg-[#efefef] h-full flex justify-center items-center'>
+      <div className="border-[1px] border-[#e1e1e1] w-1/3 rounded-md p-3 bg-white h-[80%]">
+      <h1 className='font-rowdies text-xl'>User Settings</h1>
+      </div>
+    </div>
   )
 }
 
