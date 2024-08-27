68a572205fd6f95bd2fbf2b5db956e9081e44c7f
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-08-27 13:43:55 +0100

    Fixed layout

--- frontend/src/components/ChatWindow.jsx
+++ frontend/src/components/ChatWindow.jsx
@@ -5,10 +5,13 @@ import {
   setMessages,
 } from "../redux/chatReducer/chatAction";
 import { IoMdArrowBack } from "react-icons/io";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAuth } from "../lib/hooks/useAuth";
 import { toast } from "sonner";
 import { IoSend } from "react-icons/io5";
+import { collection, onSnapshot } from "firebase/firestore";
+import { db } from "../lib/firebaseConfig";
+import { FaArrowDown } from "react-icons/fa6";
 
 const ChatWindow = ({
   activeChat,
@@ -20,9 +23,11 @@ const ChatWindow = ({
   const [fetchingMsgs, setFetchingMsgs] = useState(true);
   const [messageBody, setMessageBody] = useState("");
   const user = useAuth().user;
+  const messagesEndRef = useRef(null);
 
   useEffect(() => {
     const fetchMessages = async () => {
+      setFetchingMsgs(true);
       if (activeChat) {
         const idToken = await user.getIdToken(true);
         const response = await fetch(
@@ -39,9 +44,10 @@ const ChatWindow = ({
         if (response.status == 200) {
           const data = await response.json();
           setMessages(data.messages);
-          console.log(data.messages);
           setFetchingMsgs(false);
-        } else {
+          scrollToBottom();
+        } else if (response.status == null) {
+          console.log(response.statusText);
           setFetchingMsgs(false);
           toast.error("Error fetching messages");
           console.log("Error fetching messages");
@@ -51,6 +57,40 @@ const ChatWindow = ({
 
     fetchMessages();
   }, [activeChat]);
+
+  // useEffect(() => {
+  //   if (activeChat) {
+  //     const unsubscribe = onSnapshot(
+  //       collection(db, "chats", activeChat.chatId, "messages"),
+  //       (querySnapshot) => {
+  //         console.log("snapshot", querySnapshot.docs[0]);
+  //         const newDocument = querySnapshot
+  //           .docChanges()
+  //           .find((change) => change.type === "added");
+
+  //         if (newDocument) {
+  //           console.log(newDocument.doc.data());
+  //         }
+  //       }
+  //     );
+  //     return () => {
+  //       unsubscribe();
+  //     };
+  //   }
+  // }, [messages]);
+
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      scrollToBottom();
+    }, 0);
+
+    return () => clearTimeout(timeout);
+  }, [messages]);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   const sendMessage = async (e) => {
     e.preventDefault();
@@ -73,6 +113,26 @@ const ChatWindow = ({
     }
   };
 
+  function convertTimestampToTime(timestamp) {
+    const date = new Date(
+      timestamp._seconds * 1000 + timestamp._nanoseconds / 1000000
+    );
+
+    if (date.getHours() < 12) {
+      return date.toLocaleTimeString("en-US", {
+        hour12: true,
+        hour: "numeric",
+        minute: "numeric",
+      });
+    } else {
+      return date.toLocaleTimeString("en-US", {
+        hour12: true,
+        hour: "numeric",
+        minute: "numeric",
+      });
+    }
+  }
+
   return (
     <section className="h-full flex flex-col relative">
       {!activeChat ? (
@@ -81,7 +141,7 @@ const ChatWindow = ({
         </p>
       ) : (
         <div className="flex flex-col relative h-full">
-          <div className="bg-white p-1 h-[55px] border-b-[#e1e1e1] border-b-[1px] flex items-center font-poppins overflow-hidden">
+          <div className="bg-white p-1 h-[8vh] shrink-0 border-b-[#e1e1e1] border-b-[1px] flex items-center font-poppins">
             <button
               onClick={() => {
                 clearActiveChat();
@@ -103,31 +163,54 @@ const ChatWindow = ({
               </div>
             </div>
           </div>
-          <div className="h-full  p-2 relative flex flex-col">
+          <div className="h-[92vh] max-h-[92vh] p-2 relative flex flex-col overflow-hidden">
             {fetchingMsgs ? (
               <div className="loader-black absolute right-[50%] bottom-[50%] translate-x-[50%]"></div>
             ) : (
               <>
-                <div className="flex-grow">
-                  {messages && messages.length > 0 ? (
-                    <div className="bg-red-300 h-full flex items-end">
-                      {
-                        messages.map((message)=>{
-                          if (message.senderId == activeChat.participantId){
-                            (<div>{message.message}</div>)
-                          }
-                        })
+                {messages && messages.length > 0 ? (
+                  <div className="overflow-y-auto h-[77vh] flex flex-col gap-5 px-3">
+                    {messages.map((message) => {
+                      if (message.senderId != activeChat.participantId) {
+                        return (
+                          <div
+                            key={message.id}
+                            className="self-end max-w-[70%] relative w-fit"
+                          >
+                            <div className="shadow-sm bg-white border-[1px] border-[#bdbdbd] rounded-full p-3">
+                              {message.message}
+                            </div>
+                            <small className="absolute right-3 w-[80px] text-right">
+                              {convertTimestampToTime(message.timeStamp)}
+                            </small>
+                          </div>
+                        );
+                      } else {
+                        return (
+                          <div
+                            key={message.id}
+                            className="self-start max-w-[70%] relative"
+                          >
+                            <div className="shadow-sm bg-[#313131] text-white border-[1px] border-[#bdbdbd] rounded-full p-3">
+                              {message.message}
+                            </div>
+                            <small className="absolute left-3 w-[80px] text-left">
+                              {convertTimestampToTime(message.timeStamp)}
+                            </small>
+                          </div>
+                        );
                       }
-                    </div>
-                  ) : (
-                    <p className="flex h-full justify-center items-center">
-                      No messages found
-                    </p>
-                  )}
-                </div>
+                    })}
+                    <div ref={messagesEndRef} />
+                  </div>
+                ) : (
+                  <p className="flex h-full justify-center items-center">
+                    No messages found
+                  </p>
+                )}
                 <form
                   onSubmit={(e) => sendMessage(e)}
-                  className="p-5 w-full bg-gren-400"
+                  className=" w-full h-[15vh] flex justify-center items-center relative"
                 >
                   <div className="rounded-full lg:w-[80%] mx-auto border-[1px] border-[#bdbdbd] flex items-center">
                     <input
@@ -145,6 +228,13 @@ const ChatWindow = ({
                       <IoSend />
                     </button>
                   </div>
+                  <button
+                    type="button"
+                    onClick={scrollToBottom}
+                    className="absolute right-1 lg:right-5 p-3 rounded-full bg-[#313131]"
+                  >
+                    <FaArrowDown color="white" />
+                  </button>
                 </form>
               </>
             )}
