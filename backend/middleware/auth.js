5a2cec2f93d3bb5b318502c5978e26002062ca85
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-09-28 10:44:10 +0100

    commit

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ export const verifyToken = async (req, res, next) => {
   try {
     let token = req.header("Authorization");
     if (!token) return res.status(403).send({ message: "Access Denied" });
-    if (token.startsWith("Bearer ")) {
+    if (token.startsWith("Bearer ")) {  
       token = token.split(' ')[1];
     }
     
